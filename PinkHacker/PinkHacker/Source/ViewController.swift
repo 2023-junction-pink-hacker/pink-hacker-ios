3ce9bede44fabf8a15b495365951e856cf1b9fd1
jujube0 <kians2@naver.com>
2023-08-19 19:53:16 +0900

    [Feat] 공통 네트워크 요청 로직 추가

--- PinkHacker/PinkHacker/Source/ViewController.swift
+++ PinkHacker/PinkHacker/Source/ViewController.swift
@@ -6,11 +6,35 @@
 //
 
 import UIKit
+import SnapKit
+import Combine
 
 class ViewController: UIViewController {
+    var label: UILabel!
+    var cancellable: AnyCancellable?
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        cancellable = TestAPIRequest().publisher().sink(receiveCompletion: { _ in },
+                                      receiveValue: { [weak self] response in
+            self?.label.text = response.name
+        })
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let label = UILabel()
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        self.label = label
     }
+    
 }
