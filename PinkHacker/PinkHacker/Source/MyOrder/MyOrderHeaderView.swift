8bb2ab454638af44992f4d9ba530181aef2deb21
Jaeyong <56102421+samsung-ga@users.noreply.github.com>
2023-08-20 05:00:20 +0900

    [Feat] MyOrder 화면 구현 (#15)

---
+++ PinkHacker/PinkHacker/Source/MyOrder/MyOrderHeaderView.swift
@@ -0,0 +1,214 @@
+//
+//  MyOrderHeaderView.swift
+//  PinkHacker
+//
+//  Created by Woody Lee on 2023/08/20.
+//
+
+import Combine
+import UIKit
+import SnapKit
+import Then
+
+enum MyOrderProgress {
+    case received
+    case cooking
+    case finished
+}
+
+final class MyOrderHeaderView: UIView {
+    private let imageView = UIImageView()
+    private let descriptionLabel = UILabel()
+    private let nameLabel = UILabel()
+    private let leftTimeLabel = UILabel()
+    private let progressBar = UIView()
+    private let receivedLabel = UILabel()
+    private let cookingLabel = UILabel()
+    private let finishLabel = UILabel()
+    
+    private var bag = Set<AnyCancellable>()
+    
+    private var viewModel: ViewModel?
+    
+    struct ViewModel {
+        var name: String
+        var progress: MyOrderProgress
+        var endTime: String
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupAttribute()
+        setupLayout()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        imageView.image = viewModel.progress.image
+        nameLabel.setText(viewModel.name, attributes: Const.pizzaNameAttributes)
+        receivedLabel.setText("Received", attributes: Const.foodStageAttributes)
+        cookingLabel.setText("Cooking", attributes: Const.foodStageAttributes)
+        finishLabel.setText("Finish", attributes: Const.foodNotStageAttributes)
+        
+        switch viewModel.progress {
+        case .finished: break
+        case .cooking:
+            finishLabel.setText("Finish", attributes: Const.foodNotStageAttributes)
+        case .received:
+            cookingLabel.setText("Cooking", attributes: Const.foodNotStageAttributes)
+            finishLabel.setText("Finish", attributes: Const.foodNotStageAttributes)
+        }
+        
+        updateLeftTimeLabel()
+    }
+    
+    private func updateLeftTimeLabel() {
+        if let leftTime = viewModel?.leftTime {
+            leftTimeLabel.setText(leftTime, attributes: Const.timeAttributes)
+        }
+    }
+}
+
+extension MyOrderHeaderView {
+    private func setupAttribute() {
+        descriptionLabel.setText("My menu", attributes: Const.descriptionAttributes)
+        progressBar.backgroundColor = Const.progressbarColor
+    }
+    
+    private func setupLayout() {
+        addSubview(imageView)
+        addSubview(descriptionLabel)
+        addSubview(nameLabel)
+        addSubview(leftTimeLabel)
+        addSubview(progressBar)
+        
+        UIStackView().do {
+            $0.alignment = .firstBaseline
+            $0.distribution = .equalSpacing
+            $0.axis = .horizontal
+            
+            $0.addArrangedSubview(receivedLabel)
+            $0.addArrangedSubview(cookingLabel)
+            $0.addArrangedSubview(finishLabel)
+            
+            addSubview($0)
+            $0.snp.makeConstraints { make in
+                make.top.equalTo(progressBar.snp.bottom).offset(10)
+                make.horizontalEdges.equalToSuperview()
+                make.bottom.equalToSuperview()
+            }
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.size.equalTo(102)
+        }
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView)
+            make.leading.equalTo(imageView.snp.trailing).offset(16)
+        }
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(descriptionLabel)
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(4)
+        }
+        leftTimeLabel.snp.makeConstraints { make in
+            make.leading.equalTo(descriptionLabel)
+            make.top.equalTo(nameLabel.snp.bottom).offset(12)
+        }
+        progressBar.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(27)
+            make.height.equalTo(12)
+            make.horizontalEdges.equalToSuperview()
+        }
+        progressBar.layer.cornerRadius = 6
+        progressBar.layer.masksToBounds = true
+    }
+    
+    private func bind() {
+        Timer
+            .publish(every: 1.0, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self else { return }
+                guard let window, window.frame.intersects(self.convert(self.bounds, to: window))
+                else { return }
+                self.updateLeftTimeLabel()
+            }
+            .store(in: &bag)
+    }
+}
+
+extension MyOrderProgress {
+    var image: UIImage? {
+        switch self {
+        case .received: return .ic_progress_1
+        case .cooking: return .ic_progress_2
+        case .finished: return .ic_progress_3
+        }
+    }
+}
+
+extension MyOrderHeaderView.ViewModel {
+    var leftTime: String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        guard let endTime = dateFormatter.date(from: self.endTime) else { return nil }
+
+        guard Date.now <= endTime else { return nil }
+        
+        let leftDuration = DateInterval(start: Date.now, end: endTime).duration
+        let dayInterval: TimeInterval = 86400 // 60 x 60 x 1000
+        
+        if (leftDuration / dayInterval) >= 1 {
+            let components = Calendar.current.dateComponents([.day], from: Date.now, to: endTime)
+            return "\(components.day ?? 0)일"
+        }
+        
+        let total: Int = Int(leftDuration)
+        let hour = (total / 3600) - 9 // GMT 9시간을 빼줍니다.
+        let minute = (hour + total % 3600) / 60
+        
+        return String(format: "%02dm", minute)
+    }
+}
+
+private extension MyOrderHeaderView {
+    enum Const {
+        static let timeAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.semibold),
+            size: 18,
+            textColor: Const.timeColor
+        )
+        static let descriptionAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.semibold),
+            size: 13,
+            textColor: Const.descriptionColor
+        )
+        static let pizzaNameAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.bold),
+            size: 24,
+            textColor: Const.blackColor
+        )
+        static let foodStageAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.bold),
+            size: 14,
+            textColor: Const.blackColor
+        )
+        static let foodNotStageAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.bold),
+            size: 14,
+            textColor: Const.grayColor
+        )
+        static let descriptionColor = UIColor(red: 0.76, green: 0.76, blue: 0.74, alpha: 1)
+        static let blackColor = UIColor(red: 0.19, green: 0.19, blue: 0.19, alpha: 1)
+        static let timeColor = UIColor(red: 1, green: 0.52, blue: 0.37, alpha: 1)
+        static let grayColor = UIColor(red: 0.85, green: 0.84, blue: 0.81, alpha: 1)
+        static let progressbarColor = UIColor(red: 0.96, green: 0.96, blue: 0.93, alpha: 1)
+    }
+}
---
+++ PinkHacker/PinkHacker/Source/MyOrder/MyOrderViewController.swift
@@ -0,0 +1,182 @@
+//
+//  MyOrderViewController.swift
+//  PinkHacker
+//
+//  Created by Woody Lee on 2023/08/20.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class MyOrderViewController: UIViewController {
+    typealias Section = MyOrderSection
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Section.Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Section.Item>
+    
+    private let titleLabel = UILabel()
+    private let headerView = MyOrderHeaderView()
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    private lazy var dataSource = dataSource(of: collectionView)
+    
+    private var sections: [Section] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAttribute()
+        setupLayout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        applySections([
+            .init(type: .recent, items: [
+                .myRecent(.init(id: 0, title: "Tuesday Pizza", subtitle: "08.20 FRI")),
+                .myRecent(.init(id: 1, title: "Special Combo Combo", subtitle: "06.12 SAT"))
+            ]),
+            .init(type: .recipe, items: [
+                .myRecipe(.init(id: 0, title: "Wednesday Pizza", subtitle: "80 orders")),
+                .myRecipe(.init(id: 1, title: "Wednesday Pizza", subtitle: "80 orders")),
+                .myRecipe(.init(id: 2, title: "Wednesday Pizza", subtitle: "80 orders"))
+            ])
+        
+        ])
+    }
+    
+    func dataSource(of collectionView: UICollectionView) -> DataSource {
+        DataSource(
+            collectionView: collectionView,
+            cellProvider: { [weak self] collectionView, indexPath, item in
+                guard let self else { return nil }
+                switch item {
+                case let .myRecent(viewModel):
+                    guard let cell = collectionView.dequeueCell(RecentOrderCell.self, for: indexPath)
+                    else { return nil }
+                    cell.configure(viewModel: viewModel)
+                    cell.didTapButtonPublisher
+                        .sink { _ in }
+                        .store(in: &cell.bag)
+                    return cell
+                case let .myRecipe(viewModel):
+                    guard let cell = collectionView.dequeueCell(MyRecipeCell.self, for: indexPath)
+                    else { return nil }
+                    cell.configure(viewModel: viewModel)
+                    cell.didTapButtonPublisher
+                        .sink { _ in }
+                        .store(in: &cell.bag)
+                    return cell
+                }
+            },
+            supplementaryViewProvider: { [weak self] collectionView, elementKind, indexPath in
+                guard let section = self?.sections[safe: indexPath.section] else { return .init() }
+                guard let header = collectionView.dequeueSupplementaryView(MyOrderTitleHeaderView.self, for: indexPath)
+                else { return nil }
+                header.configure(title: section.type.headerTitle)
+                return header
+            }
+        )
+    }
+    
+    private func applySections(_ sections: [Section]) {
+        self.sections = sections
+        var snapshot = Snapshot()
+        snapshot.appendSections(sections)
+        sections.forEach { snapshot.appendItems($0.items, toSection: $0) }
+        dataSource.apply(snapshot)
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return .init { section, _ in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: NSCollectionLayoutDimension.fractionalWidth(1),
+                heightDimension: NSCollectionLayoutDimension.estimated(76)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: itemSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 12
+            section.contentInsets = .init(top: 0, leading: 20, bottom: 0, trailing: 20)
+            let headerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(62)
+            )
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top
+            )
+            section.boundarySupplementaryItems = [sectionHeader]
+            return section
+        }.then {
+            let configuration = UICollectionViewCompositionalLayoutConfiguration()
+            configuration.scrollDirection = .vertical
+            $0.configuration = configuration
+        }
+    }
+}
+
+extension MyOrderViewController {
+    private func setupAttribute() {
+        collectionView.do {
+            $0.registerCell(RecentOrderCell.self)
+            $0.registerCell(MyRecipeCell.self)
+            $0.registerSupplementaryView(MyOrderTitleHeaderView.self)
+            $0.setCollectionViewLayout(createLayout(), animated: false)
+            $0.backgroundColor = Const.backgroundColor
+            $0.contentInset = .init(top: 0, left: 0, bottom: 20, right: 0)
+        }
+        view.backgroundColor = Const.backgroundColor
+        titleLabel.setText("My Order", attributes: Const.titleAttributes)
+        headerView.configure(
+            viewModel: .init(
+                name: "I hate monday Pizza",
+                progress: .cooking,
+                endTime: "3:00"
+            )
+        )
+    }
+    
+    private func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(headerView)
+        view.addSubview(collectionView)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+            make.leading.equalToSuperview().offset(24)
+        }
+        headerView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(44)
+            make.directionalHorizontalEdges.equalToSuperview().inset(24)
+        }
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom).offset(32)
+            make.directionalHorizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+private extension MyOrderViewController {
+    enum Const {
+        static let titleAttributes: NSMutableAttributedString.PHAttributes = .init(
+            weight: .gellix(.bold),
+            size: 28,
+            textColor: Const.titleColor
+        )
+        static let titleColor = UIColor(red: 0.19, green: 0.19, blue: 0.19, alpha: 1)
+        static let backgroundColor = UIColor(red: 0.99, green: 0.98, blue: 0.97, alpha: 1)
+    }
+}
+
+extension UICollectionViewDiffableDataSource {
+    public convenience init(
+        collectionView: UICollectionView,
+        cellProvider: @escaping CellProvider,
+        supplementaryViewProvider: @escaping SupplementaryViewProvider
+    ) {
+        self.init(collectionView: collectionView, cellProvider: cellProvider)
+        self.supplementaryViewProvider = supplementaryViewProvider
+    }
+}
